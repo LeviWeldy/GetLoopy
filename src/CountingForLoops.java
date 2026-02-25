bf879a0581c3b7859549e3e704822ad93396ee1a
LeviWeldy <weldylb@mail.uc.edu>
2026-02-25 10:16:54 -0500

    Added CountingForLoops, Problems 1-4 for lab 7, Get Loopy

---
+++ src/CountingForLoops.java
@@ -0,0 +1,38 @@
+public static void main(String[] args) {
+
+
+    //Counting For Loops #1
+    System.out.println("Problem #1: Counting For Loops");
+    for (int i = 0; i <= 30; i++) {
+        System.out.print(i + " ");
+    }
+
+    System.out.println(""); // print a new line for better formatting
+    System.out.println("");
+
+
+    //Counting For Loops #2
+    System.out.println("Problem #2: Counting For Loops");
+    for (int i = 30; i >= 0; i--) {
+        System.out.print(i + " ");
+    }
+
+    System.out.println(""); // print a new line for better formatting
+    System.out.println("");
+
+//Counting For Loops #3
+    System.out.println("Problem #3: Counting For Loops");
+    for (int i = 0; i <= 18; i++, i++, i++) {
+        System.out.print(i + " ");
+    }
+
+    System.out.println(""); // print a new line for better formatting
+    System.out.println("");
+
+
+    //Counting For Loops #4
+    System.out.println("Problem #4: Counting For Loops");
+    for (int i = 10; i >= 0; i--, i--) {
+        System.out.print(i + " ");
+    }
+}
